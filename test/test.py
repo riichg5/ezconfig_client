ff2c0bcec2f12cfd2f55df2df961a444676a5075
libo <libo@talefun.com>
2023-09-05 16:22:47 +0800

    fixed

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# import sys
+# import os
+#
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from ezconfig_client import loader
 

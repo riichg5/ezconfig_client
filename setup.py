dfc0b5fbc9a6d01522c9c3d07951988d2098dbe5
libo <libo@talefun.com>
2023-09-05 19:11:33 +0800

    0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ezconfig-client",
-    version="0.3",
+    version="0.4",
     author="RiichTT",
     author_email="tolibo@hotmail.com",
     description="ezconfig client",
